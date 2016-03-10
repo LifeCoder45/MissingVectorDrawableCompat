8b3d3f024db31ac71973835e7bd58ebca3b1ecb5
Josh O'Neal <joshua.oneal@careotter.com>
2016-03-10 10:20:21 -0600

    Initial commit

---
+++ MissingVectorDrawableCompat/MainActivity.cs
@@ -0,0 +1,28 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.App;
+
+namespace MissingVectorDrawableCompat
+{
+	[Activity (
+		Label = "MissingVectorDrawableCompat", 
+		MainLauncher = true, 
+		Icon = "@mipmap/icon",
+		Theme = "@style/Theme.AppCompat.Light.NoActionBar")]
+	public class MainActivity : AppCompatActivity
+	{
+		protected override void OnCreate (Bundle savedInstanceState)
+		{
+			base.OnCreate (savedInstanceState);
+
+			SetContentView (Resource.Layout.Main);
+
+			SetSupportActionBar(FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar));
+
+			SupportActionBar.Title = "Testing";
+		}
+	}
+}
+
+
